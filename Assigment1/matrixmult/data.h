cb236d92e7092c7a0d653367172508a7710bcdfe
RamiroCope <s161601@student.dtu.dk>
2017-01-04 16:55:09 +0100

    cblas

--- Assigment1/matrixmult/data.h
+++ Assigment1/matrixmult/data.h
@@ -1,7 +1,7 @@
 #ifndef __DATA_H
 #define __DATA_H
 
-void initializeMM(double* restrict, double* restrict, int, int, int);
+void initializeMM(double*, double*, int, int, int);
 
 double ** malloc_2d(int, int);
 void free_2d(double **);
--- Assigment1/matrixmult/matmult_nat.c
+++ Assigment1/matrixmult/matmult_nat.c
@@ -1,53 +1,12 @@
-#include "data.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "matmult_nat.h"
 
-void matrix_mul(double* A, double* B, double* C, int m, int n, int k){
+void matmult_nat(int m, int n, int k, double** A, double** B, double** C){
     for (int i = 0; i < m; i++){
         for (int j = 0; j < n; j++){
-            C[i * n + j] = 0;
+            C[i][j] = 0;
             for (int rc = 0; rc < k; rc++){
-                C[i * n + j] += A[i * k + rc] * B[rc * n + j];
+                C[i][j] += A[i][rc] * B[rc][j];
             }
         }
     }
-}
-
-int main(){
-    int m = 3, n = 2, k = 5;
-    double** A = malloc_2d(m, k);
-    double** B = malloc_2d(k, n);
-    double** C = malloc_2d(m, n);
-    initializeMM(*A, *B, m, n, k);
-    matrix_mul(*A, *B, *C, m, n, k);
-
-    printf("A\n");
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < k; j++){
-            printf("%f\t", A[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("B\n");
-    for (int i = 0; i < k; i++){
-        for (int j = 0; j < n; j++){
-            printf("%f\t", B[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("C\n");
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            printf("%f\t", C[i][j]);
-        }
-        printf("\n");
-    }
-
-    free_2d(A);
-    free_2d(B);
-    free_2d(C);
-
-    return(0);
 }
