0343c61595f8605548f69af0ef632e1349cc16ee
Georghios Papoutsakis <g.papoutsakis86@gmail.com>
2017-01-05 10:18:13 +0100

    delete unecessary files

--- Assigment1/matrixmult/matmult_lib.c
+++
@@ -1,5 +0,0 @@
-#include <cblas.h>
-
-void matmult_lib(int m, int n, int k,double** A, double** B, double** C){
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1.0, &A[0][0], k, &B[0][0], n, 0.0, &C[0][0], n);
-}
--- Assigment1/matrixmult/matmult_nat.h
+++
@@ -1,4 +0,0 @@
-#ifndef __MATMULT_NAT_H
-#define __MATMULT_NAT_H
-void matmult_nat(int m, int n, int k,double** A, double** B, double** C);
-#endif  
