418b673e6611bd287ea014bcb4e7a4bd4fb54259
RamiroCope <s161601@student.dk.dtu>
2017-01-09 14:53:16 +0100

    pi

--- week2/picalc.c
+++ week2/picalc.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+double integ(double x){
+   return(4 / (1 + pow(x, 2)));
+}
 
 int main(){
-  int pi =  0;
-  int N  = 10;
-  int sum = 0;
-  int i;
-    for (i=1; i<N+1 ; N++){
-      sum += 4 / (1 + (pow((i - 0.5)/N, 2)));
+  double pi =  0;
+  int N  = 100;
+  double sum = 0;
+  double x;
+  double cpu_time;
+  clock_t T1, T2;
+
+  T1 = clock();
+
+  for (int i=1; i<N+1 ; i++){
+    x = (i - 0.5)/N;
+    sum += integ(x);
   }
+
+  T2 = clock();
+  cpu_time = ((double)(T2-T1))/CLOCKS_PER_SEC;
+
   pi = sum / N;
-  return (pi);
+  printf("%f\n", pi);
+  printf("%f\n", cpu_time);
+  return (0);
 }
