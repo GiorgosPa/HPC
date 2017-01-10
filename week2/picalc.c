379e030492210fdec9b4b2c1a700b61e10ce5b77
RamiroCope <s161601@student.dtu.dk>
2017-01-10 15:46:17 +0100

    pi

--- week2/picalc.c
+++ week2/picalc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include <omp.h>
 
 double integ(double x){
    return(4 / (1 + pow(x, 2)));
@@ -9,24 +9,24 @@ double integ(double x){
 
 int main(){
   double pi =  0;
-  int N  = 100;
+  int N  = 100000000;
   double sum = 0;
   double x;
-  double cpu_time;
-  clock_t T1, T2;
+  double local = 0.0;
+  int i;
 
-  T1 = clock();
-
-  for (int i=1; i<N+1 ; i++){
-    x = (i - 0.5)/N;
-    sum += integ(x);
-  }
-
-  T2 = clock();
-  cpu_time = ((double)(T2-T1))/CLOCKS_PER_SEC;
+  #pragma omp parallel private(local,i,x)
+  {
+    #pragma omp for
+    for (i=1; i<N+1 ; i++){
+      x = (i - 0.5)/N;
+      local += integ(x);
+    }
+    #pragma omp critical
+    sum += local;
+  } /* end opm parallel */
 
   pi = sum / N;
   printf("%f\n", pi);
-  printf("%f\n", cpu_time);
   return (0);
 }
