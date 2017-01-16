ec60ba3b78075a22165598456c03f6a43c8b8dec
Georgios Papoutsakis <g.papoutsakis86@gmail.com>
2017-01-16 14:23:16 +0100

    hello world cuda

---
+++ week3/main.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+__global__
+void hello(){
+    int block, thread, gthread, threads_per_block;
+    block = blockIdx.x;
+    thread = threadIdx.x;
+    threads_per_block = blockDim.x;
+    gthread = block * threads_per_block + thread;
+    printf("Hello world! I’m thread %d out of %d in block %d. My global thread id is %d out of %d\n",
+     thread, threads_per_block, block, gthread, threads_per_block);
+}
+
+
+int main(){
+    return(0);
+}
