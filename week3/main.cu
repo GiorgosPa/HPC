0361e316e01290df6021a360a3d828f6875451fd
Georgios Papoutsakis <g.papoutsakis86@gmail.com>
2017-01-16 14:30:54 +0100

    update main

--- week3/main.cu
+++ week3/main.cu
@@ -2,13 +2,14 @@
 
 __global__
 void hello(){
-    int block, thread, gthread, threads_per_block;
+    int block, blocks, thread, gthread, threads_per_block;
     block = blockIdx.x;
     thread = threadIdx.x;
     threads_per_block = blockDim.x;
     gthread = block * threads_per_block + thread;
+    blocks = gridDim.x;
     printf("Hello world! I’m thread %d out of %d in block %d. My global thread id is %d out of %d\n",
-     thread, threads_per_block, block, gthread, threads_per_block);
+     thread, threads_per_block, block, gthread, threads_per_block*);
 }
 
 
