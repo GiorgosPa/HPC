1560cd92a020d0f1fe784f1651149dfab2190893
Georgios Papoutsakis <g.papoutsakis86@gmail.com>
2017-01-16 14:34:10 +0100

    hello world exercise part 2

--- week3/main.cu
+++ week3/main.cu
@@ -8,6 +8,12 @@ void hello(){
     threads_per_block = blockDim.x;
     gthread = block * threads_per_block + thread;
     blocks = gridDim.x;
+    if (gthread == 100)
+    {
+        // segmentation fault
+        int *a = (int*) 0x10000;
+        *a = 0;
+    }
     printf("Hello world! I’m thread %d out of %d in block %d. My global thread id is %d out of %d\n",
      thread, threads_per_block, block, gthread, threads_per_block*blocks);
 }
